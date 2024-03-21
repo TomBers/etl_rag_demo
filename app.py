3c3c876833ecf832985f7114ddf719937234d4c8
TomBers <tjcberman@gmail.com>
2024-03-21 12:29:20 +0000

    got streaming working

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 import os
 from typing import List
 
-from langchain.vectorstores.pinecone import Pinecone as PineconeVectorStore
-from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores.pinecone import Pinecone
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.memory import ChatMessageHistory, ConversationBufferMemory
-from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+
+from langchain.prompts import  ChatPromptTemplate
 from langchain.docstore.document import Document
-from pinecone import Pinecone
+
 import chainlit as cl
 
-pc = Pinecone(
-        api_key=os.environ.get("PINECONE_API_KEY")
-    )
 
-index_name = "etl"
-
-# Optional
-namespace = None
-
+INDEX_NAME = "etl"
 EMBEDDING_MODEL_NAME = "text-embedding-3-small"
 
-embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL_NAME)
 
 # welcome_message = "Looking to purchase energy efficient products or get impartial advice on making energy savings?  Ask anything about the UK Energy Technology List (ETL), one of the world\\'s largest databases of energy-saving technology. Includes sustainability information on 8,000 tested and assessed energy efficient products, including boilers, electric motors, air conditioning and refrigeration equipment."
 welcome_message = """Looking to purchase energy efficient products or get impartial advice on making energy savings?  
@@ -33,42 +26,27 @@ We have limited space but also need plenty of refrigerated storage for food and 
 async def start():
     await cl.Message(content=welcome_message).send()
     
-    docsearch = PineconeVectorStore.from_existing_index(
-        index_name=index_name, embedding=embeddings, namespace=namespace
+    vectorstore = Pinecone.from_existing_index(
+    index_name=INDEX_NAME,
+    embedding=OpenAIEmbeddings(model=EMBEDDING_MODEL_NAME),
+    namespace=None
     )
+    retriever = vectorstore.as_retriever()
 
-    message_history = ChatMessageHistory()
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are an expert on the energy efficiency of products to increase the efficiency of buildings, you have a store of effieienct products the user would like to know more about to help make purchasing decisions."),
+        ("system", "Answer the question based only on the following context: {context}"),
+        ("user", "Question: {question}")
+    ])
 
-    memory = ConversationBufferMemory(
-        memory_key="chat_history",
-        output_key="answer",
-        chat_memory=message_history,
-        return_messages=True,
+
+    model = ChatOpenAI(model="gpt-4-0125-preview")
+    output_parser = StrOutputParser()
+
+    setup_and_retrieval = RunnableParallel(
+        {"context": retriever, "question": RunnablePassthrough()}
     )
-    
-        
-    general_system_template = (r""" 
-        Act like an expert that is focused on sustainability and who is an expert in the procurement of the energy-efficient products listed in your context, including boilers, electric motors, air conditioning and refrigeration equipment.  These products are provided by the Energy Technology List (ETL), which is a UK government-approved list of high-performance, energy-efficient products that meet the criteria of the Department for Energy Security and Net Zero.  Your purpose is to help businesses discover, investigate, compare this energy-saving equipment that is listed in your context, so as to help the user reduce carbon emissions for society at large. Your users will likely be energy managers, procurement professionals, facilities managers, and others similar industry professionals.  You should help guide your users on their journey to learn more about the products on the Energy Technology List (ETL).  Assume that all questions relate to the ETL and the products in your context. If users inquire about topics or information not covered within these instructions, kindly inform them of your dedicated focus; then, you might say: I am here to assist with questions directly related to the Energy Technology List (ETL). Then tell them about the ETL in less than 20 words.  Then educate them about the energy-efficient products listed in your context, such as boilers, electric motors, air conditioning and refrigeration equipment. Whenever it makes sense, describe a specific product from the ETL and the benefits in terms of energy efficiency. If you mention a product from the ETL, then give some specific details about that product from your context. Foster an environment for detailed, thoughtful engagement with the user. Always provide short, concise, authoritative responses, prioritizing brevity without sacrificing clarity or detail in your guidance. Always mention a product from the ETL in your reply. At the end of your reply, always ask this: How can I assist you further? 
-        ----
-        {context}
-        ----
-        """)
-    
-    general_user_template = "Question:```{question}```"
-    messages = [
-                SystemMessagePromptTemplate.from_template(general_system_template),
-                HumanMessagePromptTemplate.from_template(general_user_template)
-    ]
-    qa_prompt = ChatPromptTemplate.from_messages( messages )
-
-    chain = ConversationalRetrievalChain.from_llm(
-        ChatOpenAI(model_name="gpt-4-0125-preview", temperature=0, streaming=True),
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        combine_docs_chain_kwargs={"prompt": qa_prompt},
-        memory=memory,
-        return_source_documents=True,
-    )
+    chain = setup_and_retrieval | prompt | model | output_parser
     cl.user_session.set("chain", chain)
 
 
@@ -77,28 +55,8 @@ async def main(message: cl.Message):
     msg = cl.Message(content="")
     await msg.send()
     
-    chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
+    chain = cl.user_session.get("chain")
+    for chunk in chain.stream(message.content):  
+        await msg.stream_token(chunk)
 
-    cb = cl.AsyncLangchainCallbackHandler()
-
-    res = await chain.ainvoke(message.content, callbacks=[cb])
-    answer = res["answer"]
-    source_documents = res["source_documents"]  # type: List[Document]
-
-    text_elements = []  # type: List[cl.Text]
-
-    if source_documents:
-        for source_idx, source_doc in enumerate(source_documents):
-            source_name = f"source_{source_idx}"
-            # Create the text element referenced in the message
-            text_elements.append(
-                cl.Text(content=source_doc.page_content, name=source_name)
-            )
-        source_names = [text_el.name for text_el in text_elements]
-
-        if source_names:
-            answer += f"\nSources: {', '.join(source_names)}"
-        else:
-            answer += "\nNo sources found"
-
-    await cl.Message(content=answer, elements=text_elements).send()
+    await cl.Message(content="🤖🤖🤖").send()
